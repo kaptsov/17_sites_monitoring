2e899b52338a3e2fc7de507e360e9fe371a34541
kaptsov <kaptsovaa@gmail.com>
2018-03-19 15:50:09 +0900

    refactored

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,6 +4,14 @@ import http1
 import datetime
 
 
+def get_url_list():
+    try:
+        url_list_file = get_commandline_args().filepath
+        return load_urls4check(url_list_file)
+    except(FileNotFoundError, TypeError):
+        return None
+
+
 def get_commandline_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('filepath')
@@ -29,27 +37,31 @@ def get_domain_expire_date(url):
     return expiration_date
 
 
-def get_expiration_days(expiration_date):
-    now = datetime.datetime.now()
-    expiration = expiration_date - now
-    return expiration
+def get_domain_status(url, month=31):
+    try:
+        expire_date = get_domain_expire_date(url)
+        now = datetime.datetime.now()
+        expiration = (expire_date - now).days
+        state = expiration > month
+        return expiration, state
+    except whois.parser.PywhoisError:
+        return None, None
+
+
+def print_results(url_list):
+    for url in url_list:
+        domain_expiration, domain_state = get_domain_status(url)
+        if domain_state is not None:
+            print(url, 'responds with status 200:', is_server_respond_200(url))
+            print('Status is OK:', domain_state,
+                  'Domain will expire in', domain_expiration, 'days.')
+        else:
+            print('Cant find', url)
 
 
 if __name__ == '__main__':
-    try:
-        url_list_file = get_commandline_args().filepath
-        url_list = load_urls4check(url_list_file)
-    except(FileNotFoundError, TypeError):
-        exit('File not found or empty')
-
-    month = 31
-    for url in url_list:
-        try:
-            expire_date = get_domain_expire_date(url)
-            expiration = get_expiration_days(expire_date).days
-            domain_status = expiration > month
-            print(url, 'responds with status 200:', is_server_respond_200(url))
-            print('Status is OK:', domain_status,
-                  'Domain will expire in', expiration, 'days.')
-        except whois.parser.PywhoisError:
-            print('Cant find', url)
+    url_list = get_url_list()
+    if url_list:
+        print_results(url_list)
+    else:
+        print('File not found or empty')
